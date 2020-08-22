5588a7a481e4d40cf93a4b757add3f997b3eaee0
Denis Glotov <denisglotov98@mail.ru>
2020-08-22 20:36:00 +0300

    init

---
+++ request.go
@@ -0,0 +1,40 @@
+package httptestutil
+
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type Router struct {
+	router *gin.Engine
+}
+
+func NewRouter(router *gin.Engine) *Router {
+	return &Router{router: router}
+}
+
+func (r *Router) Request(t *testing.T, method, path string, bodyJson *string, headers map[string]string) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(method, path, body(bodyJson))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for header, v := range headers {
+		req.Header.Set(header, v)
+	}
+
+	rr := httptest.NewRecorder()
+	r.router.ServeHTTP(rr, req)
+	return rr
+}
+
+func body(json *string) io.Reader {
+	if json == nil {
+		return nil
+	}
+	return bytes.NewBuffer([]byte(*json))
+}
